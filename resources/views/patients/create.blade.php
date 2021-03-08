547854aa2e3e3f434a520ab3f1822491bdcdac2b
devvahagn@gmail.com <devvahagn@gmail.com>
2021-03-08 18:05:00 +0400

    'a'

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -27,7 +27,7 @@
                 @csrf
                 <fieldset>
                     <div class="mb-3">
-                        <label for="full_name" class="form-label">Full name *</label>
+                        <label for="full_name" class="form-label">ФИО *</label>
                         <input type="text" id="full_name" class="form-control" name="full_name"
                                value="{{old('full_name')}}">
 
@@ -52,7 +52,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="birth_day" class="form-label">Birth day *</label>
+                        <label for="birth_day" class="form-label">Дата рождения *</label>
                         <input type="date" id="birth_day" class="form-control" name="birth_day"
                                value="{{old('birth_day')}}">
 
@@ -65,7 +65,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="sex" class="form-label">Sex *</label>
+                        <label for="sex" class="form-label">Пол *</label>
                         <select class="form-control" id="sex" name="sex">
                             <option>Choose</option>
                             <option value="male" {{old('sex') === 'male'?'selected':''}}>Male</option>
@@ -81,7 +81,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="passport_number" class="form-label">Passport number *</label>
+                        <label for="passport_number" class="form-label">Паспорт *</label>
                         <input type="text" id="passport_number" class="form-control" name="passport_number" value="{{old('passport_number')}}">
 
                         @error('passport_number')
@@ -92,7 +92,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="flight" class="form-label">Flight</label>
+                        <label for="flight" class="form-label">Рейс</label>
                         <input type="text" id="flight" class="form-control" name="flight" value="{{old('flight')}}">
 
                         @error('flight')
@@ -103,7 +103,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="flight_date" class="form-label">Flight date</label>
+                        <label for="flight_date" class="form-label">Дата рейса</label>
                         <input type="datetime-local" id="flight_date" class="form-control"
                                name="flight_date" value="{{old('flight_date')}}">
 
@@ -116,8 +116,7 @@
 
 
                     <div class="mb-3">
-                        <label for="date_and_time_of_sample_collection" class="form-label">Date and time of sample
-                            collection *</label>
+                        <label for="date_and_time_of_sample_collection" class="form-label">Дата и время взятия пробы *</label>
                         <input type="datetime-local" id="date_and_time_of_sample_collection" class="form-control"
                                name="date_and_time_of_sample_collection" value="{{old('date_and_time_of_sample_collection')}}">
 
@@ -130,8 +129,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="date_and_time_of_result_report" class="form-label">Date and time of result
-                            report *</label>
+                        <label for="date_and_time_of_result_report" class="form-label">Дата и время получения результата *</label>
                         <input type="datetime-local" id="date_and_time_of_result_report" class="form-control"
                                name="date_and_time_of_result_report" value="{{old('date_and_time_of_result_report')}}">
 
