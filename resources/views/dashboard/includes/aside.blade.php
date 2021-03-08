d941c68903ca86ebd1de3168d026e5056ec9ec7e
devvahagn@gmail.com <devvahagn@gmail.com>
2021-03-08 20:30:38 +0400

    'a'

--- resources/views/dashboard/includes/aside.blade.php
+++ resources/views/dashboard/includes/aside.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="/dashboard" class="brand-link">
         <img src="/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">NWS LAB</span>
+        <span class="brand-text font-weight-light">Euromed</span>
     </a>
 
     <div class="sidebar">
